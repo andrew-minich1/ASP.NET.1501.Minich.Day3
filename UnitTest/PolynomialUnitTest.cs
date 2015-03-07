e46682e0376617ed75f62f2a9e348e5f17fdd86d
Andrew Minich <andrew.minich1@gmail.com>
2015-03-07 20:28:41 +0300

    ASP.NET.Dey3

---
+++ UnitTest/PolynomialUnitTest.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiMember;
+
+namespace Opertor_UnitTest
+{
+    [TestClass]
+    public class PolynomialUnitTest
+    {
+        #region Operator +
+        [TestMethod]
+        public void AdditionPolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(2, 3, 4);
+            Polynomial p2 = new Polynomial(5, 2, 3, 4);
+            Polynomial p3 = p1 + p2;
+            Assert.AreEqual("5x^3+4x^2+6x+8", p3.ToString());
+        }
+        [TestMethod]
+        public void AdditionPolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(2, 3, 4);
+            Polynomial p2 = new Polynomial(0, 0, 0, 0);
+            Polynomial p3 = p1 + p2;
+            Assert.AreEqual("2x^2+3x+4", p3.ToString());
+        }
+
+        [TestMethod]
+        public void AdditionPolynomialTestMethod3()
+        {
+            Polynomial p1 = new Polynomial(0, 0, 0);
+            Polynomial p2 = new Polynomial(0, 0, 0, 0);
+            Polynomial p3 = p1 + p2;
+            Assert.AreEqual("0", p3.ToString());
+        }
+
+        [TestMethod]
+        public void AdditionPolynomialTestMethod4()
+        {
+            Polynomial p1 = new Polynomial(0, 0, 1);
+            Polynomial p2 = new Polynomial(1, 0, 0, 0);
+            Polynomial p3 = p1 + p2;
+            Assert.AreEqual("x^3+1", p3.ToString());
+        }
+
+        [TestMethod]
+        public void AdditionPolynomialTestMethod5()
+        {
+            Polynomial p1 = new Polynomial(7, -5, -1);
+            Polynomial p2 = new Polynomial(-2, 1, 2, 6);
+            Polynomial p3 = p1 + p2;
+            Assert.AreEqual("-2x^3+8x^2-3x+5", p3.ToString());
+        }
+        #endregion
+
+        #region Operator -
+        [TestMethod]
+        public void SubstractPolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(6);
+            Polynomial p2 = new Polynomial(2, 5);
+            Polynomial p3 = p1 - p2;
+            Assert.AreEqual("-2x+1", p3.ToString());
+        }
+        [TestMethod]
+        public void SubstractPolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(4, 3, 2, 8);
+            Polynomial p2 = new Polynomial(3, 4, 5);
+            Polynomial p3 = p1 - p2;
+            Assert.AreEqual("4x^3-2x+3", p3.ToString());
+        }
+        [TestMethod]
+        public void SubstractPolynomialTestMethod3()
+        {
+            Polynomial p1 = new Polynomial(0, 0, 0);
+            Polynomial p2 = new Polynomial(0, 0, 0);
+            Polynomial p3 = p1 - p2;
+            Assert.AreEqual("0", p3.ToString());
+        }
+        #endregion
+
+        #region Operator *
+        [TestMethod]
+        public void MultiplyPolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(7, 5, -6, -4);
+            Polynomial p2 = new Polynomial(-1, 5, 0, 3);
+            Polynomial p3 = p1 * p2;
+            Assert.AreEqual("-7x^6+30x^5+31x^4-5x^3-5x^2-18x-12", p3.ToString());
+        }
+        [TestMethod]
+        public void MultiplyPolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(7, 5, -6, -4);
+            Polynomial p2 = new Polynomial(0);
+            Polynomial p3 = p1 * p2;
+            Assert.AreEqual("0", p3.ToString());
+        }
+        [TestMethod]
+        public void MultiplyPolynomialTestMethod3()
+        {
+            Polynomial p1 = new Polynomial(1);
+            Polynomial p2 = new Polynomial(-1, 5, 0, 3);
+            Polynomial p3 = p1 * p2;
+            Assert.AreEqual("-1x^3+5x^2+3", p3.ToString());
+        }
+        #endregion
+
+        #region Operator == !=
+        [TestMethod]
+        public void EqualsPolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(5, 4, 3);
+            Polynomial p2 = new Polynomial(5, 4, 3);
+            bool result = p1 == p2;
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void EqualsPolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(5, 4, 3);
+            Polynomial p2 = new Polynomial(5, 4, 3);
+            bool result = p1 != p2;
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void EqualsPolynomialTestMethod3()
+        {
+            Polynomial p1 = new Polynomial(5, 5, 3);
+            Polynomial p2 = new Polynomial(5, 4, 3);
+            bool result = p1 == p2;
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void EqualsPolynomialTestMethod4()
+        {
+            Polynomial p1 = new Polynomial(5, 3);
+            Polynomial p2 = new Polynomial(5, 4, 3);
+            bool result = p1 != p2;
+            Assert.AreEqual(true, result);
+        }
+        #endregion
+
+        #region Derivative
+        [TestMethod]
+        public void DerivativePolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(5, 4, 3);
+            Polynomial result = p1.Derivative();
+            Assert.AreEqual("10x+4", result.ToString());
+        }
+
+        [TestMethod]
+        public void DerivativePolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(3, 1, -5, 0, 3);
+            Polynomial result = p1.Derivative();
+            Assert.AreEqual("12x^3+3x^2-10x", result.ToString());
+        }
+        #endregion
+
+        #region Indexer
+        [TestMethod]
+        public void IndexerPolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(2, 5, 4, 3);
+            p1[0]=0;
+            Assert.AreEqual("5x^2+4x+3", p1.ToString());
+        }
+        #endregion
+
+        #region Calculate
+        [TestMethod]
+        public void CalculatePolynomialTestMethod1()
+        {
+            Polynomial p1 = new Polynomial(2, 2, 0, -10);
+            double result = p1.Calculate(2);
+            Assert.AreEqual(14, result);
+        }
+
+        [TestMethod]
+        public void CalculatePolynomialTestMethod2()
+        {
+            Polynomial p1 = new Polynomial(2, 2, 0, -10);
+            double result = p1.Calculate(0);
+            Assert.AreEqual(-10, result);
+        } 
+        #endregion
+
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Opertor+UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Opertor+UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("1b706298-c5c9-48f8-a797-5022c6bbe30f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
